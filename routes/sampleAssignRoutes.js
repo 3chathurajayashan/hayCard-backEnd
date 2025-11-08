f5e7b5ea5900cf085e96eafd88525037b1d351fe
chathura_jayashan <chathurachamod88@gmail.com>
2025-11-08 21:33:33 +0530

    f

--- routes/sampleAssignRoutes.js
+++ routes/sampleAssignRoutes.js
@@ -10,12 +10,12 @@ const {
 
 const SampleAssign = require("../models/sampleAssignModel");
 
-// ✅ Multer setup
+ /*
 const storage = multer.diskStorage({
   destination: (req, file, cb) => cb(null, "uploads/"),
   filename: (req, file, cb) =>
     cb(null, Date.now() + path.extname(file.originalname)),
-});
+});*/
 
 const upload = multer({
   storage,
