4b81d305c153038605c154a9e83a6a1b089df2c5
chathura_jayashan <chathurachamod88@gmail.com>
2025-11-09 13:52:45 +0530

    f

--- api/models/SampleAssign.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const sampleAssignSchema = new mongoose.Schema({
-  referenceNumber: { type: String, required: true },
-  documentUrl: { type: String, required: true },
-}, { timestamps: true });
-
-export default mongoose.models.SampleAssign ||
-  mongoose.model("SampleAssign", sampleAssignSchema);
