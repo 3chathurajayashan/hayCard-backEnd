d1bdd12cd02e70a5a96b61bd64c950279c0784ea
chathura_jayashan <chathurachamod88@gmail.com>
2025-11-09 14:45:46 +0530

    f

--- api/index.js
+++ api/index.js
@@ -35,11 +35,14 @@ app.use(
         callback(new Error("Not allowed by CORS"));
       }
     },
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   })
 );
+
+// Handle preflight requests
+app.options("*", cors());
  
 app.use(bodyParser.json());
 app.use("/uploads", express.static("uploads"));
